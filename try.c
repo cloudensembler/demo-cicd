6188405ec87e287cfbaa0546b572b6f773e174da
Sameer <snatu@cavium.com>
2019-02-20 04:49:19 -0800

    Code Changed to "Hello World"

--- try.c
+++ try.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int i, val=40;
-    for(i=0; i<val; i++)
-    {
-        printf("Hellow world -- %d\n", i);
-    }
+	char *str = "Hello World";
+	printf("%s\n",str);
 }
